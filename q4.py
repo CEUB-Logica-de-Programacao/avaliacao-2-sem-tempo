d3f09d3afb650a98b4649d1f5d129a025def9f19
Ericoreto <111282811+Ericoreto@users.noreply.github.com>
2022-11-07 09:34:07 -0300

    Update q4.py

--- q4.py
+++ q4.py
@@ -27,17 +27,17 @@
 def q4(numeral):
     # Escreva seu código aqui
     objetivo = 6
-roma = ["I": 1, "V" : 5, 'X' : 10,'L' : 50,'C' : 100, 'D' : 500, 'M' : 1000]
-I = 1
-V = 5
-X = 10
-L = 50
-C = 100
-D = 500
-M = 100
+    roma = ["I": 1, "V" : 5, 'X' : 10,'L' : 50,'C' : 100, 'D' : 500, 'M' : 1000]
+    I = 1
+    V = 5
+    X = 10
+    L = 50
+    C = 100
+    D = 500
+    M = 100
 
 
-print()
+    print()
 
 
 if __name__ == '__main__':
