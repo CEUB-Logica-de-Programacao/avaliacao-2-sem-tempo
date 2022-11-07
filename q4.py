95ba5e43c0f0986fc88ee11aee5beac9768cd790
Ericoreto <111282811+Ericoreto@users.noreply.github.com>
2022-11-07 09:34:52 -0300

    Update q4.py

--- q4.py
+++ q4.py
@@ -26,19 +26,7 @@
 
 def q4(numeral):
     # Escreva seu código aqui
-    objetivo = 6
-    roma = ["I": 1, "V" : 5, 'X' : 10,'L' : 50,'C' : 100, 'D' : 500, 'M' : 1000]
-    I = 1
-    V = 5
-    X = 10
-    L = 50
-    C = 100
-    D = 500
-    M = 100
-
-
-    print()
-
+    
 
 if __name__ == '__main__':
     print(q4('MCMXCIV'))  # 1994
